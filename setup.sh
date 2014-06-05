e4f10c78c2a19b58b0390035e3ddb1408d236d44
Gary Doran, Jr <garydoranjr@gmail.com>
2014-06-05 15:08:29 -0400

    add BNT to setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-URL=http://people.tuebingen.mpg.de/kzhang/KCI-test.zip
-ZIPFILE=KCI-test.zip
+KCITURL=http://people.tuebingen.mpg.de/kzhang/KCI-test.zip
+KCITZIPFILE=KCI-test.zip
+BNTURL=https://bnt.googlecode.com/files/FullBNT-1.0.7.zip
+BNTZIPFILE=FullBNT-1.0.7.zip
 
-wget $URL -O $ZIPFILE
-unzip $ZIPFILE algorithms/* gpml-matlab/* CI_PERM/*
-rm $ZIPFILE
+wget $KCITURL -O $KCITZIPFILE
+unzip $KCITZIPFILE algorithms/* gpml-matlab/* CI_PERM/*
+rm $KCITZIPFILE
+
+wget --no-check-certificate $BNTURL -O $BNTZIPFILE
+unzip $BNTZIPFILE bnt/*
+rm $BNTZIPFILE
